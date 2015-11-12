d221aef5d764d089b8143f54ccfe2e884561245c
JackMackerel <ludichrislyts@gmail.com>
2015-11-11 18:14:38 -0800

    more formatting

--- sites/all/modules/ang/angular-listing.tpl.php
+++ sites/all/modules/ang/angular-listing.tpl.php
@@ -1,12 +1,12 @@
 <div ng-app="nodeListing">
 
    <div ng-controller="ListController">
-
+    
      <h3>Filter</h3>
      <input ng-model="search" ng-change="doSearch()">
 
       <ul style="list-style:none">
-        <li ng-repeat="node in nodes">{{ node.title }}</br/><button ng-click="open(node.nid)">Open</button></li>
+        <li ng-repeat="node in nodes"><button style="width:100%;" ng-click="open(node.nid)">{{ node.title }}</button></li>
       </ul>
 
      <script type="text/ng-template" id="loadedNodeTemplate">
